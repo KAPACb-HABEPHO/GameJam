4c91268d8f737293ce897d4a1bf572df49d71c3d
grustno <120631702+grustno@users.noreply.github.com>
2023-02-14 19:00:00 +0300

    скинул пустой скрипт

---
+++ Assets/Scripts/RobotScripts/RobotScripts.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class RobotScripts : MonoBehaviour
+{
+
+
+
+    void Start()
+    {
+
+    }
+   
+    void Update()
+    {
+
+    }
+
+
+}
